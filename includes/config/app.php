81fcd9487c22d4c848fe91dadfcf1138b99bf8c0
abbasShDev <abbas20alzaeem@gmail.com>
2020-11-12 09:55:33 +0300

    change config app url

--- includes/config/app.php
+++ includes/config/app.php
@@ -3,7 +3,7 @@
 $config = [
 
     'app_name' => 'Inventory System',
-    'app_url' => 'https://b5121fec3565.ngrok.io/Inventory_System/',
+    'app_url' => 'http://localhost:8080/Inventory_System/',
     'admin_email' => 'admin@inventorysystem.com',
     'root_dir'      => $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'Inventory_System/',
 
